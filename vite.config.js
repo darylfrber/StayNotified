edfde1d90331ac6b1abd7f0525566e4ec2297afd
darylfrber <134004@student.horizoncollege.nl>
2025-03-22 03:53:03 +0100

    vite config change

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,14 @@ import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
+    server: {
+        host: 'calendar-app',
+        port: 5173,
+        strictPort: true,
+        hmr: {
+            host: 'calendar-app',
+        },
+    },
     plugins: [
         laravel({
             input: 'resources/js/app.js',
